41d041c891e3c74f5de029894287ba5c1683b02d
Cameron Brantley <brantley.cameron@gmail.com>
2020-12-01 20:53:33 -0600

    Initial commit

---
+++ components/Nav.js
@@ -0,0 +1,10 @@
+export default () => `
+<nav>
+    <i class="fas fa-bars"></i>
+    <ul class="hidden--mobile nav-links">
+      <li><a href="index.html">Home</a></li>
+      <li><a href="bio.html">Bio</a></li>
+      <li><a href= "gallery.html">Gallery</a></li>
+      <li><a href="register.html">Register</a></li>
+    </ul>
+  </nav>`
